ec22bc0407faddae685f3c0a1eb2d4c83088f3d8
Karthik-B-06 <karthik06051996@gmail.com>
2021-09-13 13:16:06 +0530

    chore: :white_check_mark: added Box primitive component tests

---
+++ src/primitives/Box/__tests__/Box.test.tsx
@@ -0,0 +1,57 @@
+import { render } from '@testing-library/react-native';
+import React from 'react';
+import { Box } from '../../Box';
+import { Text } from '../../Text';
+
+describe('basic', () => {
+  it('should render correctly', () => {
+    const component = render(<Box />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should assign a ref', () => {
+    const ref = React.createRef();
+    render(
+      <Box ref={ref}>
+        <Text>Hello world</Text>
+      </Box>
+    );
+    expect(ref.current).toMatchSnapshot();
+  });
+
+  it('should render correctly with a style prop', () => {
+    const component = render(
+      <Box
+        style={{
+          backgroundColor: 'tomato',
+          padding: 4,
+          margin: 10,
+        }}
+      />
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render children correctly', () => {
+    const component = render(
+      <Box
+        style={{
+          backgroundColor: 'tomato',
+          padding: 4,
+          margin: 10,
+        }}
+      >
+        <Text
+          style={{
+            color: 'white',
+            textAlign: 'center',
+            fontSize: 18,
+          }}
+        >
+          This is a Box with Text Inside
+        </Text>
+      </Box>
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
