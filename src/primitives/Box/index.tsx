5bd965fe94833a16bb89c5b4719b07088b567a41
Karthik-B-06 <karthik06051996@gmail.com>
2021-09-27 16:05:16 +0530

    fix: :rotating_light: fixed {} being used as a type lint error

--- src/primitives/Box/index.tsx
+++ src/primitives/Box/index.tsx
@@ -1,7 +1,8 @@
 import { View, ViewProps as RNViewProps } from 'react-native';
+import type { Dict } from 'src/utils/types';
 import { createComponent } from '../../utils/createComponent';
 
-export type LibraryBoxProps = {};
+export type LibraryBoxProps = Dict<unknown>;
 
 export type BoxProps = RNViewProps & LibraryBoxProps;
 
