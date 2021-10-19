2c31c2f0f1579c8b6fe3f51dca4aa8e5651a148a
Karthik-B-06 <karthik06051996@gmail.com>
2021-10-19 17:48:34 +0530

    refactor: :recycle: moved switch related constants to switch Theme

--- src/theme/defaultTheme/switch.ts
+++ src/theme/defaultTheme/switch.ts
@@ -12,4 +12,32 @@ export const switchTheme = {
     lg: 'h-[17px] bg-white rounded-full shadow-md',
     xl: 'w-6 h-6 bg-white rounded-full shadow-md',
   },
+
+  switchInterpolateWidths: {
+    sm: [11, 14, 14, 11],
+    md: [13, 15, 15, 13],
+    lg: [17, 20, 20, 17],
+    xl: [24, 27, 27, 24],
+  },
+
+  thumbIntermediateTranslateValue: {
+    sm: 3,
+    md: 2,
+    lg: 3,
+    xl: 3,
+  },
+
+  thumbTranslateValue: {
+    sm: 24 - 11 - 1.5,
+    md: 26 - 13 - 1.5,
+    lg: 32 - 17 - 1.5,
+    xl: 48 - 24 - 2,
+  },
+
+  thumbInitTranslateValue: {
+    sm: 1.5,
+    md: 1.5,
+    lg: 1.5,
+    xl: 2,
+  },
 };
