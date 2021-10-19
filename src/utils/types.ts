7088635f90696bd06751ae0467cd07b97c2e18da
Karthik-B-06 <karthik06051996@gmail.com>
2021-10-19 16:59:25 +0530

    feat: :label: added DeepDictionary and DeepPartial common types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,1 +1,11 @@
 export type Dict<T = unknown> = Record<string, T>;
+
+export type DeepDictionary<K> = {
+  [P in keyof K]: K[P] extends Dict
+    ? DeepDictionary<K[P]> & { [x: string]: unknown }
+    : K[P];
+};
+
+export type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>;
+};
