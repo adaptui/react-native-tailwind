d89caf797777191298871a52967069040d3d638b
Karthik-B-06 <karthik06051996@gmail.com>
2021-10-20 15:58:00 +0530

    chore: :recycle: replaced useTailwindThemeContext with useTheme

--- example/App/stories/AnimatedBox.stories.tsx
+++ example/App/stories/AnimatedBox.stories.tsx
@@ -1,18 +1,19 @@
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import {
-  interpolate, useAnimatedStyle,
+  interpolate,
+  useAnimatedStyle,
   useSharedValue,
   withDelay,
   withRepeat,
-  withTiming
-} from "react-native-reanimated";
-import { useTailwindThemeContext } from 'react-native-system';
+  withTiming,
+} from 'react-native-reanimated';
+import { useTheme } from 'react-native-system';
 import { AnimatedBox, Box } from '../../../src/primitives';
 
 const AnimatedRingComponent = () => {
   const boxRef = React.createRef<typeof Box>();
-  const tailwind = useTailwindThemeContext();
+  const tailwind = useTheme();
 
   // Animation Code
   const ring = useSharedValue(0);
@@ -40,10 +41,22 @@ const AnimatedRingComponent = () => {
     );
   }, []);
   return (
-    <Box ref={boxRef} style={tailwind.style('items-center justify-center flex-1')}>
-      <AnimatedBox style={[tailwind.style('absolute m-10 h-20 w-20 rounded-md border-2 border-red-600'), ringStyle]} />
+    <Box
+      ref={boxRef}
+      style={tailwind.style('items-center justify-center flex-1')}
+    >
+      <AnimatedBox
+        style={[
+          tailwind.style(
+            'absolute m-10 h-20 w-20 rounded-md border-2 border-red-600'
+          ),
+          ringStyle,
+        ]}
+      />
     </Box>
   );
 };
 
-storiesOf('Animated Box', module).add('Square Loop Animation', () => <AnimatedRingComponent />);
+storiesOf('Animated Box', module).add('Square Loop Animation', () => (
+  <AnimatedRingComponent />
+));
--- example/App/stories/Touchable.stories.tsx
+++ example/App/stories/Touchable.stories.tsx
@@ -2,7 +2,7 @@ import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import { Alert } from 'react-native';
-import { useTailwindThemeContext } from 'react-native-system';
+import { useTheme } from 'react-native-system';
 import { Text } from '../../../src/primitives/Text';
 import { Touchable } from '../../../src/primitives/Touchable';
 
@@ -14,7 +14,7 @@ const actions = {
 };
 
 const TouchableComponent = () => {
-  const tailwind = useTailwindThemeContext();
+  const tailwind = useTheme();
   return (
     <Touchable
       onPress={actions.onPressTouchable}
