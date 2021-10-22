3710c7221873b3ba4b5ccb498ae64e9215d4d14e
Karthik-B-06 <karthik06051996@gmail.com>
2021-10-22 11:29:49 +0530

    feat: :art: added progress bar theme to default theme context

--- src/theme/defaultTheme/index.ts
+++ src/theme/defaultTheme/index.ts
@@ -1,9 +1,11 @@
 import { badge } from './badge';
+import { progress } from './progress';
 import { switchTheme } from './switch';
 
 const extendedTheme = {
   badge,
   switchTheme,
+  progress,
 };
 
 export default extendedTheme;
