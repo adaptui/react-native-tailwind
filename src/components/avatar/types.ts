8484279516af4d2ae2615fe7439de9539bf70ead
Karthik-B-06 <karthik06051996@gmail.com>
2021-11-13 23:59:16 +0530

    feat: :label: added avatar component types

---
+++ src/components/avatar/types.ts
@@ -0,0 +1,32 @@
+import { ImageProps, ImageSourcePropType } from 'react-native';
+
+export type AvatarSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl';
+export type AvatarStatusType = 'active' | 'away' | 'sleep' | 'typing' | null;
+
+export interface AvatarImageBasicProps {
+  imageProps: Omit<ImageProps, 'source'>;
+  src: ImageSourcePropType;
+  handleFallback: () => void;
+}
+
+export interface AvatarBasicProps {
+  size: AvatarSizes;
+  circular: boolean;
+}
+
+export type AvatarImageProps = AvatarImageBasicProps &
+  Pick<AvatarBasicProps, 'circular'>;
+
+export interface AvatarStatusProps {
+  status: AvatarStatusType;
+}
+
+export interface AvatarOtherProps {
+  name: string;
+}
+
+export interface AvatarProps
+  extends AvatarBasicProps,
+    AvatarImageBasicProps,
+    AvatarStatusProps,
+    AvatarOtherProps {}
