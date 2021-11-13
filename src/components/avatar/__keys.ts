5b8b2542e9b3f513416f19391203ceefaf503150
Karthik-B-06 <karthik06051996@gmail.com>
2021-11-14 00:00:28 +0530

    feat: :pencil2: added avatar component prop keys

---
+++ src/components/avatar/__keys.ts
@@ -0,0 +1,12 @@
+export const AVATAR_IMAGE_KEYS = [
+  'imageProps',
+  'src',
+  'handleFallback',
+  'circular',
+];
+
+export const AVATAR_BASIC_KEYS = ['size', 'circular', 'name'];
+
+export const AVATAR_OTHER_KEYS = ['name'];
+
+export const AVATAR_STATUS_KEYS = ['status'];
