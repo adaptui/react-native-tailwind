b4ed58cb27f0f1be9b0e4a98d5b0441396c530ca
Karthik-B-06 <karthik06051996@gmail.com>
2021-11-22 19:09:16 +0530

    refactor: :recycle: destructuring of props to assign default values

--- src/components/avatar/AvatarProps.tsx
+++ src/components/avatar/AvatarProps.tsx
@@ -25,20 +25,15 @@ interface AvatarPropsReturnType {
 export const useAvatarProps = (
   props: React.PropsWithChildren<Partial<AvatarProps>>
 ): AvatarPropsReturnType => {
-  let _props = { ...props };
   const avatarGroupProps = useAvatarGroup();
-  if (!_props.size) {
-    _props = { ..._props, size: avatarGroupProps.size || 'xl' };
-  }
-  if (!_props.circular) {
-    _props = {
-      ..._props,
-      circular: avatarGroupProps && avatarGroupProps.circular,
-    };
-  }
-  const _imageProps = pick(_props, AVATAR_IMAGE_KEYS) as AvatarImageProps;
-  const _basicProps = pick(_props, AVATAR_BASIC_KEYS) as AvatarBasicProps;
-  const _otherProps = pick(_props, AVATAR_OTHER_KEYS) as AvatarOtherProps;
-  const _statusProps = pick(_props, AVATAR_STATUS_KEYS) as AvatarStatusProps;
+  props = {
+    size: avatarGroupProps?.size || 'xl',
+    circular: avatarGroupProps?.circular,
+    ...props,
+  };
+  const _imageProps = pick(props, AVATAR_IMAGE_KEYS) as AvatarImageProps;
+  const _basicProps = pick(props, AVATAR_BASIC_KEYS) as AvatarBasicProps;
+  const _otherProps = pick(props, AVATAR_OTHER_KEYS) as AvatarOtherProps;
+  const _statusProps = pick(props, AVATAR_STATUS_KEYS) as AvatarStatusProps;
   return { _imageProps, _basicProps, _otherProps, _statusProps };
 };
