c442051bb1333918252fee3f1a3e4ae3b2aacba0
Karthik-B-06 <karthik06051996@gmail.com>
2021-11-23 14:08:00 +0530

    style: :art: added spinner styles to default theme

---
+++ src/theme/defaultTheme/spinner.ts
@@ -0,0 +1,15 @@
+export const spinner = {
+  base: 'border-2 border-solid rounded-full border-gray-800',
+  stroke: {
+    transparent: 'border-b-transparent border-l-transparent',
+    visible: 'border-b-gray-300 border-l-gray-300',
+  },
+  label: 'sr-only',
+  size: {
+    xs: 'h-3 w-3',
+    sm: 'h-[14px] w-[14px]',
+    md: 'h-4 w-4',
+    lg: 'h-5 w-5',
+    xl: 'h-6 w-6',
+  },
+};
