4f794940ed2a4ab4afc62cbbf91d570f4f621d8c
Karthik-B-06 <karthik06051996@gmail.com>
2021-11-23 19:14:16 +0530

    chore: :zap: added button options to hook

--- src/components/button/ButtonProps.tsx
+++ src/components/button/ButtonProps.tsx
@@ -1,15 +1,30 @@
 import { pick } from 'lodash';
-import { ButtonProps } from './buttonTypes';
-import { BUTTON_BASIC_KEYS } from './__keys';
+import {
+  ButtonOptionProps,
+  ButtonProps,
+  ButtonLibraryProps,
+} from './buttonTypes';
+import { BUTTON_BASIC_KEYS, BUTTON_OPTION_KEYS } from './__keys';
 
 interface ButtonPropsReturnType {
-  _buttonProps: ButtonProps;
+  _buttonProps: ButtonLibraryProps;
+  _buttonOptions: ButtonOptionProps;
 }
 
 export const useButtonProps = (
   props: React.PropsWithChildren<Partial<ButtonProps>>
 ): ButtonPropsReturnType => {
   props = { size: 'md', variant: 'solid', ...props };
-  const _buttonBasicProps = pick(props, BUTTON_BASIC_KEYS) as ButtonProps;
-  return { _buttonProps: _buttonBasicProps };
+  const _buttonBasicProps = pick(
+    props,
+    BUTTON_BASIC_KEYS
+  ) as ButtonLibraryProps;
+  const _buttonOptionProps = pick(
+    props,
+    BUTTON_OPTION_KEYS
+  ) as ButtonOptionProps;
+  return {
+    _buttonProps: _buttonBasicProps,
+    _buttonOptions: _buttonOptionProps,
+  };
 };
