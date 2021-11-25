2e3b685ccdcfe487d75a3b3e97de383e14cdde83
Karthik-B-06 <karthik06051996@gmail.com>
2021-11-25 16:12:30 +0530

    feat: :zap: added <Spinner /> for loading button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Pressable } from 'react-native';
-import { Box, Text, useTheme } from 'react-native-system';
+import {
+  Box,
+  Spinner,
+  SpinnerSizes,
+  Text,
+  useTheme,
+} from 'react-native-system';
 import { useButtonProps } from './ButtonProps';
 import { ButtonProps } from './buttonTypes';
 
@@ -9,6 +15,7 @@ export const Button: React.FC<Partial<ButtonProps>> = (props) => {
   const buttonTheme = useTheme('button');
   const { _buttonProps, _buttonOptions } = useButtonProps(props);
   const iconAspectRatio = 1;
+  const isButtonDisabled = props.disabled || props.loading;
   return (
     <Pressable
       style={({ pressed }) => [
@@ -16,7 +23,7 @@ export const Button: React.FC<Partial<ButtonProps>> = (props) => {
           buttonTheme.base,
           buttonTheme.size.default[_buttonProps.size],
           buttonTheme.variant.default[_buttonProps.variant],
-          props.disabled
+          isButtonDisabled
             ? buttonTheme.variant.disabled[_buttonProps.variant]
             : '',
           pressed ? buttonTheme.variant.pressed[_buttonProps.variant] : ''
@@ -24,54 +31,66 @@ export const Button: React.FC<Partial<ButtonProps>> = (props) => {
       ]}
       {...props}
     >
-      {_buttonOptions.prefix && (
-        <Box
-          style={[
-            tailwind.style(buttonTheme.prefix[_buttonProps.size]),
-            { aspectRatio: iconAspectRatio },
-          ]}
-        >
-          {/* @ts-ignore */}
-          {React.cloneElement(_buttonOptions.prefix, {
-            stroke: tailwind.getColor(
-              props.disabled
-                ? buttonTheme.icon.variant.disabled[_buttonProps.variant]
-                : buttonTheme.icon.variant.default[_buttonProps.variant]
-            ),
-          })}
-        </Box>
-      )}
-      {typeof props.children === 'string' ? (
-        <Text
-          style={tailwind.style([
-            buttonTheme.text.size[_buttonProps.size],
-            buttonTheme.text.variant.default[_buttonProps.variant],
-            props.disabled
-              ? buttonTheme.text.variant.disabled[_buttonProps.variant]
-              : '',
-          ])}
-        >
-          {props.children}
-        </Text>
+      {_buttonOptions.loading ? (
+        <Spinner
+          size={buttonTheme.loading.size[_buttonProps.size] as SpinnerSizes}
+        />
       ) : (
-        props.children
-      )}
-      {_buttonOptions.suffix && (
-        <Box
-          style={[
-            tailwind.style(buttonTheme.suffix[_buttonProps.size]),
-            { aspectRatio: iconAspectRatio },
-          ]}
-        >
-          {/* @ts-ignore */}
-          {React.cloneElement(_buttonOptions.suffix, {
-            stroke: tailwind.getColor(
-              props.disabled
-                ? buttonTheme.icon.variant.disabled[_buttonProps.variant]
-                : buttonTheme.icon.variant.default[_buttonProps.variant]
-            ),
-          })}
-        </Box>
+        <>
+          {_buttonOptions.prefix && (
+            <Box
+              style={[
+                tailwind.style(buttonTheme.prefix[_buttonProps.size]),
+                { aspectRatio: iconAspectRatio },
+              ]}
+            >
+              {/* @ts-ignore */}
+              {React.cloneElement(_buttonOptions.prefix, {
+                stroke: tailwind.getColor(
+                  isButtonDisabled
+                    ? buttonTheme.icon.variant.disabled[_buttonProps.variant]
+                    : buttonTheme.icon.variant.default[_buttonProps.variant]
+                ),
+              })}
+            </Box>
+          )}
+          {typeof props.children === 'string' ? (
+            <Text
+              adjustsFontSizeToFit
+              allowFontScaling={false}
+              style={[
+                tailwind.style([
+                  buttonTheme.text.size[_buttonProps.size],
+                  buttonTheme.text.variant.default[_buttonProps.variant],
+                  isButtonDisabled
+                    ? buttonTheme.text.variant.disabled[_buttonProps.variant]
+                    : '',
+                ]),
+              ]}
+            >
+              {props.children}
+            </Text>
+          ) : (
+            props.children
+          )}
+          {_buttonOptions.suffix && (
+            <Box
+              style={[
+                tailwind.style(buttonTheme.suffix[_buttonProps.size]),
+                { aspectRatio: iconAspectRatio },
+              ]}
+            >
+              {/* @ts-ignore */}
+              {React.cloneElement(_buttonOptions.suffix, {
+                stroke: tailwind.getColor(
+                  isButtonDisabled
+                    ? buttonTheme.icon.variant.disabled[_buttonProps.variant]
+                    : buttonTheme.icon.variant.default[_buttonProps.variant]
+                ),
+              })}
+            </Box>
+          )}
+        </>
       )}
     </Pressable>
   );
