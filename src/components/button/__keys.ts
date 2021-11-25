3e6ec88e3e0d2412f054dc425ec9895ca14e04c0
Karthik-B-06 <karthik06051996@gmail.com>
2021-11-25 16:11:21 +0530

    chore: :label: added loading key to Button props

--- src/components/button/__keys.ts
+++ src/components/button/__keys.ts
@@ -1,2 +1,2 @@
 export const BUTTON_BASIC_KEYS = ['disabled', 'loading', 'size', 'variant'];
-export const BUTTON_OPTION_KEYS = ['prefix', 'suffix'];
+export const BUTTON_OPTION_KEYS = ['prefix', 'suffix', 'loading'];
