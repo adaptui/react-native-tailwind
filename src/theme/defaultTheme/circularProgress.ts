ec1f551e603615340ce374b23027fb07cde16a11
Karthik-B-06 <karthik06051996@gmail.com>
2021-11-29 18:14:17 +0530

    style: :art: added hint based and default sizes for circular progress

--- src/theme/defaultTheme/circularProgress.ts
+++ src/theme/defaultTheme/circularProgress.ts
@@ -1,10 +1,25 @@
 export const circularProgress = {
   progressTrackColor: 'text-gray-800',
   baseTrackColor: 'text-gray-200',
-  size: {
+  defaultSize: {
     sm: 7,
     md: 8,
     lg: 10,
     xl: 14,
+  },
+  withHintSize: {
+    sm: 22,
+    md: 28,
+    lg: 32,
+    xl: 40,
+  },
+  text: {
+    base: 'absolute font-medium text-gray-900 text-center',
+    size: {
+      sm: 'text-xs leading-tight',
+      md: 'text-sm leading-tight',
+      lg: 'text-base leading-tight',
+      xl: 'text-xl leading-tight',
+    },
   },
 };
