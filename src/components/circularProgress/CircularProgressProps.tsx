7ba6864c57a7b9b3dd9b59e25128175746249f9c
Karthik-B-06 <karthik06051996@gmail.com>
2021-11-29 13:09:58 +0530

    chore: :label: added circluar progress props hook with default values

---
+++ src/components/circularProgress/CircularProgressProps.tsx
@@ -0,0 +1,29 @@
+import { pick } from 'lodash';
+import { CircularProgressProps, useTheme } from 'react-native-system';
+import { CIRCULAR_PROGRESS_LIB_KEYS } from './__keys';
+
+interface CircularProgressPropsReturnType {
+  _circularProgressProps: CircularProgressProps;
+}
+
+export const useCircularProgressProps = (
+  props: React.PropsWithChildren<Partial<CircularProgressProps>>
+): CircularProgressPropsReturnType => {
+  const tailwind = useTheme();
+  const circularProgressTheme = useTheme('circularProgress');
+  props = {
+    size: 'md',
+    strokeWidth: 2,
+    max: 100,
+    progressTrackColor: tailwind.getColor(
+      circularProgressTheme.progressTrackColor
+    ),
+    trackColor: tailwind.getColor(circularProgressTheme.baseTrackColor),
+    ...props,
+  };
+  const _circularProgressProps = pick(
+    props,
+    CIRCULAR_PROGRESS_LIB_KEYS
+  ) as CircularProgressProps;
+  return { _circularProgressProps };
+};
---
+++ src/components/circularProgress/__keys.ts
@@ -0,0 +1,8 @@
+export const CIRCULAR_PROGRESS_LIB_KEYS = [
+  'size',
+  'strokeWidth',
+  'progressTrackColor',
+  'trackColor',
+  'value',
+  'max',
+];
