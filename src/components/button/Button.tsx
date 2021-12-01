9eedfbb1cc7384a26b9d043c1f4ae774d6ae4544
Karthik-B-06 <karthik06051996@gmail.com>
2021-12-01 18:38:40 +0530

    feat: :sparkles: updated Button component with prefix, suffix and its loading states

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Pressable } from 'react-native';
 import {
   Box,
   Spinner,
   SpinnerSizes,
   Text,
+  Touchable,
   useTheme,
 } from 'react-native-system';
 import { useButtonProps } from './ButtonProps';
+import { ButtonSpinner } from './ButtonSpinner';
 import { ButtonProps } from './buttonTypes';
 
 export const Button: React.FC<Partial<ButtonProps>> = (props) => {
@@ -16,8 +17,51 @@ export const Button: React.FC<Partial<ButtonProps>> = (props) => {
   const { _buttonProps, _buttonOptions } = useButtonProps(props);
   const iconAspectRatio = 1;
   const isButtonDisabled = props.disabled || props.loading;
+  const children = _buttonOptions.icon ? (
+    <Box
+      style={[
+        tailwind.style([
+          buttonTheme.icon.size[_buttonProps.size],
+          _buttonOptions.loading ? 'opacity-0' : '',
+        ]),
+        { aspectRatio: iconAspectRatio },
+      ]}
+    >
+      {/* @ts-ignore */}
+      {React.cloneElement(_buttonOptions.icon, {
+        stroke: tailwind.getColor(
+          isButtonDisabled
+            ? buttonTheme.icon.variant.disabled[_buttonProps.variant]
+            : buttonTheme.icon.variant.default[_buttonProps.variant]
+        ),
+      })}
+    </Box>
+  ) : typeof props.children === 'string' ? (
+    <Text
+      adjustsFontSizeToFit
+      allowFontScaling={false}
+      style={[
+        tailwind.style([
+          buttonTheme.text.size[_buttonProps.size],
+          buttonTheme.text.variant.default[_buttonProps.variant],
+          isButtonDisabled
+            ? buttonTheme.text.variant.disabled[_buttonProps.variant]
+            : '',
+          _buttonOptions.loading &&
+          !_buttonOptions.prefix &&
+          !_buttonOptions.suffix
+            ? 'opacity-0'
+            : '',
+        ]),
+      ]}
+    >
+      {props.children}
+    </Text>
+  ) : (
+    props.children
+  );
   return (
-    <Pressable
+    <Touchable
       style={({ pressed }) => [
         tailwind.style(
           buttonTheme.base,
@@ -30,20 +74,33 @@ export const Button: React.FC<Partial<ButtonProps>> = (props) => {
         ),
       ]}
       {...props}
+      disabled={isButtonDisabled}
     >
-      {_buttonOptions.loading ? (
-        <Spinner
-          size={buttonTheme.loading.size[_buttonProps.size] as SpinnerSizes}
-        />
-      ) : (
-        <>
-          {_buttonOptions.prefix && (
-            <Box
-              style={[
-                tailwind.style(buttonTheme.prefix[_buttonProps.size]),
-                { aspectRatio: iconAspectRatio },
-              ]}
-            >
+      {!_buttonOptions.prefix &&
+        !_buttonOptions.suffix &&
+        _buttonOptions.loading && (
+          <Box
+            style={tailwind.style('absolute z-10 items-center justify-center')}
+          >
+            <Spinner
+              size={buttonTheme.loading.size[_buttonProps.size] as SpinnerSizes}
+            />
+          </Box>
+        )}
+      {_buttonOptions.prefix && (
+        <Box
+          style={[
+            tailwind.style(buttonTheme.prefix[_buttonProps.size]),
+            { aspectRatio: iconAspectRatio },
+          ]}
+        >
+          {_buttonOptions.loading && !_buttonOptions.suffix ? (
+            <ButtonSpinner
+              size={_buttonProps.size}
+              spinner={_buttonOptions.spinner}
+            />
+          ) : (
+            <>
               {/* @ts-ignore */}
               {React.cloneElement(_buttonOptions.prefix, {
                 stroke: tailwind.getColor(
@@ -52,34 +109,25 @@ export const Button: React.FC<Partial<ButtonProps>> = (props) => {
                     : buttonTheme.icon.variant.default[_buttonProps.variant]
                 ),
               })}
-            </Box>
+            </>
           )}
-          {typeof props.children === 'string' ? (
-            <Text
-              adjustsFontSizeToFit
-              allowFontScaling={false}
-              style={[
-                tailwind.style([
-                  buttonTheme.text.size[_buttonProps.size],
-                  buttonTheme.text.variant.default[_buttonProps.variant],
-                  isButtonDisabled
-                    ? buttonTheme.text.variant.disabled[_buttonProps.variant]
-                    : '',
-                ]),
-              ]}
-            >
-              {props.children}
-            </Text>
+        </Box>
+      )}
+      {children}
+      {_buttonOptions.suffix && (
+        <Box
+          style={[
+            tailwind.style(buttonTheme.suffix[_buttonProps.size]),
+            { aspectRatio: iconAspectRatio },
+          ]}
+        >
+          {_buttonOptions.loading ? (
+            <ButtonSpinner
+              size={_buttonProps.size}
+              spinner={_buttonOptions.spinner}
+            />
           ) : (
-            props.children
-          )}
-          {_buttonOptions.suffix && (
-            <Box
-              style={[
-                tailwind.style(buttonTheme.suffix[_buttonProps.size]),
-                { aspectRatio: iconAspectRatio },
-              ]}
-            >
+            <>
               {/* @ts-ignore */}
               {React.cloneElement(_buttonOptions.suffix, {
                 stroke: tailwind.getColor(
@@ -88,10 +136,10 @@ export const Button: React.FC<Partial<ButtonProps>> = (props) => {
                     : buttonTheme.icon.variant.default[_buttonProps.variant]
                 ),
               })}
-            </Box>
+            </>
           )}
-        </>
+        </Box>
       )}
-    </Pressable>
+    </Touchable>
   );
 };
