eed573f3ed75b9a0557a4c3bda8d872fd1aff2f8
Karthik-B-06 <karthik06051996@gmail.com>
2021-12-14 22:21:21 +0530

    feat: :sparkles: added icon style option to createButtonIcon function

--- src/components/button/createButtonIcon.ts
+++ src/components/button/createButtonIcon.ts
@@ -1,14 +1,17 @@
+import { ViewStyle } from 'react-native';
 import { passProps, RenderPropType } from 'react-native-system';
 
 interface CreateButtonIconOptions {
   icon: RenderPropType;
   iconFill?: string;
+  iconStyle?: ViewStyle;
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const createButtonIcon = (options: CreateButtonIconOptions) => {
-  const { icon, iconFill } = options;
+  const { icon, iconFill, iconStyle } = options;
   return passProps(icon, {
-    fill: iconFill,
+    color: iconFill,
+    style: iconStyle,
   });
 };
