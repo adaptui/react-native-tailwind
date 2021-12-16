82ada09e952dbdf6f7490d3d5318c4d1e7e1c782
Karthik-B-06 <karthik06051996@gmail.com>
2021-12-16 16:45:04 +0530

    feat: :tada: added createIcon function'

---
+++ src/components/create-icon/createIcon.tsx
@@ -0,0 +1,17 @@
+import { ViewStyle } from 'react-native';
+import { passProps, RenderPropType } from 'react-native-system';
+
+interface CreateIconOptions {
+  icon: RenderPropType;
+  iconFill?: string;
+  iconStyle?: ViewStyle;
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const createIcon = (options: CreateIconOptions) => {
+  const { icon, iconFill, iconStyle } = options;
+  return passProps(icon, {
+    color: iconFill,
+    style: iconStyle,
+  });
+};
