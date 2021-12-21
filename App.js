47e753a36fda7688bc268924c17caceb0919a9f6
Karthik-B-06 <karthik06051996@gmail.com>
2021-12-21 17:27:13 +0530

    chore: :zap: wrapped StorybookUIRoot in View and fixed StatusBar issue in Android

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
+import { SafeAreaView, StatusBar, LogBox } from 'react-native';
 import StorybookUIRoot from './.ondevice/Storybook';
+import React from 'react';
+import tailwind from 'twrnc';
+LogBox.ignoreAllLogs();
 
-export default StorybookUIRoot;
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const App = () => {
+  return (
+    <SafeAreaView
+      style={tailwind.style(
+        `flex-1 android:mt-[${StatusBar.currentHeight || 0}px]`
+      )}
+    >
+      <StorybookUIRoot />
+    </SafeAreaView>
+  );
+};
+
+export default App;
