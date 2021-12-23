d493e97551a4c067fed1baaa53a00bc820fe9e21
Karthik-B-06 <karthik06051996@gmail.com>
2021-12-23 06:02:10 +0530

    chore: :truck: changed import path to the specific file

--- src/assets/svg/DefaultUser.tsx
+++ src/assets/svg/DefaultUser.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Svg, { ClipPath, Defs, G, Path, Rect } from 'react-native-svg';
-import { IconProps } from '../../index';
+import { IconProps } from '../../utils/types';
 
 export const DefaultUser: React.FC<IconProps> = ({ fill = '#52525B' }) => {
   return (
