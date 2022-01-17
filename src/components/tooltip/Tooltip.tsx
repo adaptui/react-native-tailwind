3f00927192c86431001ad25d4c3004bd25073ea5
Karthik-B-06 <karthik06051996@gmail.com>
2022-01-17 15:40:42 +0530

    chore: :sparkles: added arrow positioning, and web specific tooltip which appears on hover

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,8 +1,10 @@
+import { useHover } from '@react-native-aria/interactions';
 import {
   OverlayContainer,
   useOverlayPosition,
 } from '@react-native-aria/overlays';
-import React, { RefObject, useEffect, useRef, useState } from 'react';
+import React, { RefObject, useEffect, useMemo, useRef, useState } from 'react';
+import { Platform, StyleSheet, ViewStyle } from 'react-native';
 import {
   interpolate,
   useAnimatedStyle,
@@ -13,6 +15,7 @@ import { AnimatedBox, Box, Text } from '../../primitives';
 import { useTheme } from '../../theme';
 import { createComponent, RenderPropType } from '../../utils';
 import { composeEventHandlers, mergeRefs } from '../../utils/mergeRefs';
+import { TooltipArrow } from './TooltipArrow';
 
 const SPRING_CONFIG = {
   mass: 5,
@@ -21,6 +24,13 @@ const SPRING_CONFIG = {
   overshootClamping: false,
   restSpeedThreshold: 0.001,
   restDisplacementThreshold: 0.001,
+};
+
+const defaultArrowHeight = 12;
+const defaultArrowWidth = 12;
+
+const getDiagonalLength = (height: number, width: number) => {
+  return Math.pow(height * height + width * width, 0.5);
 };
 
 export type TooltipPlacement =
@@ -42,6 +52,7 @@ export interface TooltipProps {
   content: RenderPropType;
   containerPadding: number;
   offset: number;
+  hasArrow: boolean;
 }
 
 interface TooltipContentProps extends TooltipProps {
@@ -49,7 +60,14 @@ interface TooltipContentProps extends TooltipProps {
 }
 
 const TooltipContent: React.FC<Partial<TooltipContentProps>> = (props) => {
-  const { content, triggerRef, placement, containerPadding, offset } = props;
+  const {
+    content,
+    triggerRef,
+    placement: placementProp,
+    containerPadding,
+    offset,
+    hasArrow,
+  } = props;
   const tailwind = useTheme();
   const tooltipRef = useRef();
   const tooltipAnimation = useSharedValue(0);
@@ -67,35 +85,133 @@ const TooltipContent: React.FC<Partial<TooltipContentProps>> = (props) => {
       ],
     };
   });
-  const { overlayProps } = useOverlayPosition({
+  const { overlayProps, arrowProps, placement } = useOverlayPosition({
     overlayRef: tooltipRef,
     targetRef: triggerRef as RefObject<unknown>,
-    placement,
+    placement: placementProp,
     containerPadding,
-    offset,
+    offset: offset || props.hasArrow ? 4 : 6,
     shouldFlip: true,
     isOpen: true,
   });
+
+  const diagonalLength =
+    getDiagonalLength(defaultArrowHeight, defaultArrowWidth) / 2;
+
+  let finalArrowPlacementStyles: ViewStyle = {};
+  if (placement?.includes('top')) {
+    finalArrowPlacementStyles = {
+      ...finalArrowPlacementStyles,
+      bottom: Math.ceil((diagonalLength - defaultArrowHeight) / 2) + 2,
+    };
+  }
+
+  if (placement?.includes('bottom')) {
+    finalArrowPlacementStyles = {
+      ...finalArrowPlacementStyles,
+      top: Math.ceil((diagonalLength - defaultArrowHeight) / 2) + 2,
+    };
+  }
+
+  if (placement?.includes('left')) {
+    finalArrowPlacementStyles = {
+      ...finalArrowPlacementStyles,
+      right: Math.ceil((diagonalLength - defaultArrowWidth) / 2) + 2,
+    };
+  }
+
+  if (placement?.includes('right')) {
+    finalArrowPlacementStyles = {
+      ...finalArrowPlacementStyles,
+      left: Math.ceil((diagonalLength - defaultArrowWidth) / 2) + 2,
+    };
+  }
+
+  const arrowTransformStyles: ViewStyle = {
+    transform: [
+      {
+        translateX:
+          placement === 'top' || placement === 'bottom'
+            ? -defaultArrowWidth / 2
+            : 0,
+      },
+      {
+        translateY:
+          placement === 'left' || placement === 'right'
+            ? -defaultArrowHeight / 2
+            : 0,
+      },
+      {
+        rotate:
+          placement === 'top'
+            ? '0deg'
+            : placement === 'bottom'
+            ? '180deg'
+            : placement === 'left'
+            ? '-90deg'
+            : '90deg',
+      },
+    ],
+  };
+
+  const getContainerStyle: Partial<ViewStyle> = useMemo(() => {
+    if (placement === 'top') {
+      return { marginBottom: diagonalLength };
+    }
+
+    if (placement === 'bottom') {
+      return { marginTop: diagonalLength };
+    }
+
+    if (placement === 'left') {
+      return { marginRight: diagonalLength };
+    }
+
+    if (placement === 'right') {
+      return { marginLeft: diagonalLength };
+    }
+    return {};
+  }, [diagonalLength, placement]);
 
   return (
     <AnimatedBox
       ref={tooltipRef}
       style={[
-        tailwind.style(
-          'absolute px-2 py-1.5 bg-gray-800 rounded-lg shadow-lg '
-        ),
+        tailwind.style('absolute'),
         overlayProps.style,
         tooltipAnimatedStyle,
       ]}
     >
-      {typeof content === 'string' ? (
-        <Text
-          style={tailwind.style('text-cxs text-white font-normal leading-4')}
+      <Box
+        style={[
+          tailwind.style('px-2 py-1.5 bg-gray-800 rounded-lg'),
+          getContainerStyle,
+        ]}
+      >
+        {typeof content === 'string' ? (
+          <Text
+            style={tailwind.style(
+              'text-cxs text-white text-center font-normal leading-4'
+            )}
+          >
+            {content}
+          </Text>
+        ) : (
+          content
+        )}
+      </Box>
+      {hasArrow && (
+        <Box
+          style={[
+            StyleSheet.absoluteFill,
+            tailwind.style('w-3 h-3 z-10'),
+            arrowProps.style,
+            arrowTransformStyles,
+            finalArrowPlacementStyles,
+          ]}
         >
-          {content}
-        </Text>
-      ) : (
-        content
+          <TooltipArrow />
+        </Box>
       )}
     </AnimatedBox>
   );
@@ -106,7 +222,8 @@ export const RNTooltip: React.FC<Partial<TooltipProps>> = (props) => {
     content,
     placement = 'bottom',
     containerPadding = 0,
-    offset = 10,
+    offset = 6,
+    hasArrow = false,
   } = props;
   const [isTooltipVisible, setTooltipVisible] = useState(false);
 
@@ -115,6 +232,7 @@ export const RNTooltip: React.FC<Partial<TooltipProps>> = (props) => {
 
   const tailwind = useTheme();
   const tooltipTriggerRef = useRef();
+  const { isHovered, hoverProps } = useHover({}, tooltipTriggerRef);
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
@@ -131,22 +249,37 @@ export const RNTooltip: React.FC<Partial<TooltipProps>> = (props) => {
       props.children.onPressOut,
       hideTooltip
     ),
+    ...hoverProps,
   });
 
   return (
     <Box style={tailwind.style('relative')}>
       {TooltipTrigger}
-      {isTooltipVisible && (
-        <OverlayContainer>
-          <TooltipContent
-            triggerRef={tooltipTriggerRef}
-            content={content}
-            placement={placement}
-            offset={offset}
-            containerPadding={containerPadding}
-          />
-        </OverlayContainer>
-      )}
+      {Platform.OS === 'web'
+        ? isHovered && (
+            <OverlayContainer>
+              <TooltipContent
+                triggerRef={tooltipTriggerRef}
+                content={content}
+                placement={placement}
+                offset={offset}
+                containerPadding={containerPadding}
+                hasArrow={hasArrow}
+              />
+            </OverlayContainer>
+          )
+        : isTooltipVisible && (
+            <OverlayContainer>
+              <TooltipContent
+                triggerRef={tooltipTriggerRef}
+                content={content}
+                placement={placement}
+                offset={offset}
+                containerPadding={containerPadding}
+                hasArrow={hasArrow}
+              />
+            </OverlayContainer>
+          )}
     </Box>
   );
 };
