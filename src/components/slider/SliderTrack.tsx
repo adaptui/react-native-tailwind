732a9b495d510f5709904845427694bbc24cc956
Karthik-B-06 <karthik06051996@gmail.com>
2022-02-02 11:21:19 +0530

    chore: :sparkles: created Slider Track component

---
+++ src/components/slider/SliderTrack.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { AnimatedBox } from '../../primitives';
+import { useTheme } from '../../theme';
+import { SliderProps } from './Slider';
+
+export const SliderTrack: React.FC<Pick<SliderProps, 'size'>> = ({ size }) => {
+  const tailwind = useTheme();
+  const sliderTheme = useTheme('slider');
+
+  return (
+    <AnimatedBox
+      style={[
+        tailwind.style([
+          sliderTheme.track.common,
+          sliderTheme.track.size[size],
+        ]),
+        sliderTheme.track.position[size],
+      ]}
+    />
+  );
+};
