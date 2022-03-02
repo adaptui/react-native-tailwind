f9b37b908ac250095fa90521bcaff05cf268c97f
Karthik-B-06 <karthik06051996@gmail.com>
2022-03-02 12:15:11 +0530

    chore: :zap: wrapped App insider GestureHandlerRootView

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { LogBox, SafeAreaView, StatusBar } from 'react-native';
 import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import tailwind from 'twrnc';
 import StorybookUIRoot from './.ondevice/Storybook';
 
@@ -9,13 +10,15 @@ LogBox.ignoreAllLogs();
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const App = () => {
   return (
-    <SafeAreaView
-      style={tailwind.style(
-        `flex-1 android:mt-[${StatusBar.currentHeight || 0}px]`
-      )}
-    >
-      <StorybookUIRoot />
-    </SafeAreaView>
+    <GestureHandlerRootView style={tailwind.style('flex-1')}>
+      <SafeAreaView
+        style={tailwind.style(
+          `flex-1 android:mt-[${StatusBar.currentHeight || 0}px]`
+        )}
+      >
+        <StorybookUIRoot />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
 
