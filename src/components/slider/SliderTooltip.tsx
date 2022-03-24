7c57607b791f28ffe84c141600d84dc99fb7a62c
Karthik-B-06 <karthik06051996@gmail.com>
2022-03-24 16:19:27 +0530

    chore: :sparkles: added separate tooltip arrow component

--- src/components/slider/SliderTooltip.tsx
+++ src/components/slider/SliderTooltip.tsx
@@ -7,13 +7,23 @@ import {
   useSharedValue,
   withSpring,
 } from 'react-native-reanimated';
-import { SliderSizes } from './Slider';
+import Svg, { Path } from 'react-native-svg';
 import { AnimatedBox, Box } from '../../primitives';
 import { useTheme } from '../../theme';
 import { createComponent } from '../../utils';
 import { TooltipProps } from '../tooltip';
-import { TooltipArrow } from '../tooltip/TooltipArrow';
+import { SliderSizes } from './Slider';
 
+const TooltipArrow: React.FC = () => {
+  return (
+    <Svg width="100%" height="100%" viewBox="0 0 12 5" fill="none">
+      <Path
+        d="M11.1466 0C16.3801 0 -4.3808 0 0.853735 0C2.7834 0 4.5931 5 6.00014 5C7.40719 5 9.23748 0 11.1466 0Z"
+        fill="#27272A"
+      />
+    </Svg>
+  );
+};
 interface SliderTooltipProps extends TooltipProps {
   triggerRef: React.RefObject<unknown>;
   draggingValue: SharedValue<number>;
