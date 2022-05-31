df49841e2cede0ce5bf712af89552575397bb47a
Karthik-B-06 <karthik06051996@gmail.com>
2022-05-31 16:02:51 +0530

    chore: :sparkles: added navigation/drawer container to the example app

--- .ondevice/AppRoot.tsx
+++ .ondevice/AppRoot.tsx
@@ -1,12 +1,23 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import React from 'react';
 import { Box, Text, useTheme } from '../src';
 
-const AppRoot = () => {
+function HomeScreen() {
   const tailwind = useTheme();
   return (
     <Box style={tailwind.style('flex-1 justify-center items-center')}>
-      <Text>Hello, World</Text>
+      <Text>Hello,World from Home Screen</Text>
     </Box>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+const AppRoot = () => {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Home" component={HomeScreen} />
+    </Drawer.Navigator>
   );
 };
 
--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import tailwind from 'twrnc';
 import AppRoot from './.ondevice/AppRoot';
 import { TailwindThemeProvider } from './src';
+import { NavigationContainer } from '@react-navigation/native';
 
 LogBox.ignoreAllLogs();
 
@@ -12,15 +13,17 @@ LogBox.ignoreAllLogs();
 const App = () => {
   return (
     <GestureHandlerRootView style={tailwind.style('flex-1')}>
-      <SafeAreaView
-        style={tailwind.style(
-          `flex-1 android:mt-[${StatusBar.currentHeight || 0}px]`
-        )}
-      >
-        <TailwindThemeProvider>
-          <AppRoot />
-        </TailwindThemeProvider>
-      </SafeAreaView>
+      <NavigationContainer>
+        <SafeAreaView
+          style={tailwind.style(
+            `flex-1 android:mt-[${StatusBar.currentHeight || 0}px]`
+          )}
+        >
+          <TailwindThemeProvider>
+            <AppRoot />
+          </TailwindThemeProvider>
+        </SafeAreaView>
+      </NavigationContainer>
     </GestureHandlerRootView>
   );
 };
