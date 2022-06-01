dd35d6f053e71cedf8156ec1266e3de8c158af02
Karthik-B-06 <karthik06051996@gmail.com>
2022-06-01 17:25:33 +0530

    style: :art: updated color palette

--- src/theme/tailwind.config.js
+++ src/theme/tailwind.config.js
@@ -1,26 +1,23 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
-const colors = require("tailwindcss/colors");
+import { adaptUIColors } from './colors';
 
 module.exports = {
   theme: {
     ...defaultTheme,
+    colors: {
+      ...adaptUIColors,
+    },
     extend: {
-      colors: {
-        orange: colors.orange,
-        gray: colors.gray,
-        emarald: colors.emerald,
-        violet: colors.violet,
-      },
       fontSize: {
-        xs: "12px",
-        cxs: "13px",
-        sm: "14px",
-        base: "16px",
-        "paragraph-cxs": "13px",
-        "paragraph-sm": "14px",
-        "2base": "32px",
+        'xs': '12px',
+        'cxs': '13px',
+        'sm': '14px',
+        'base': '16px',
+        'paragraph-cxs': '13px',
+        'paragraph-sm': '14px',
+        '2base': '32px',
       },
-    }
+    },
   },
   plugins: [],
 };
