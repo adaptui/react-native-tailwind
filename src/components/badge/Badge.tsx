83876b8f5a88e5548afd9c7503cba77acfd20152
Karthik-B-06 <karthik06051996@gmail.com>
2022-06-07 12:49:51 +0530

    refactor: :recycle: removed useBadgeProps hook, added prefix & box style, text style overiding case

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,85 +1,123 @@
 import React, { forwardRef } from 'react';
-import { TextStyle, ViewStyle } from 'react-native';
-import { Box, Text } from '../../primitives';
+import { Box, BoxProps, Text, TextProps } from '../../primitives';
 import { useTheme } from '../../theme';
-import { createComponent } from '../../utils';
-import { useBadgeProps } from './BadgeProps';
+import { createComponent, cx, RenderPropType, styleAdapter } from '../../utils';
+import { createIcon } from '../create-icon';
+import { Icon } from '../icon';
 
 export type BadgeSizes = 'sm' | 'md' | 'lg';
+
 export type BadgeTheme =
   | 'primary'
   | 'secondary'
   | 'default'
   | 'success'
   | 'danger';
+
 export type BadgeVariants = 'outline' | 'solid' | 'subtle';
-export interface BadgeProps {
+export interface BadgeProps extends BoxProps {
   /**
-   * The size of the Badge component.
-   * @default lg
+   * How large should the badge be?
+   * @default md
    */
-  size: BadgeSizes;
+  size?: BadgeSizes;
   /**
-   * The themeColor of the Badge component.
+   * How the badge should be themed?
    * @default default
    */
-  themeColor: BadgeTheme;
+  themeColor?: BadgeTheme;
   /**
-   * The variant of the Badge component.
+   * How the badge should look?
    * @default solid
    */
-  variant: BadgeVariants;
-  /**
-   * The Container style of the Badge component.
-   * @default {}
-   */
-  containerStyle: ViewStyle;
+  variant?: BadgeVariants;
   /**
    * The Text style of the Badge component.
    * @default {}
    */
-  textStyle: TextStyle;
+  textProps?: TextProps;
+  /**
+   * If added, the Badge will show an icon before the tag's text.
+   */
+  prefix?: RenderPropType;
 }
 
 const RNBadge: React.FC<Partial<BadgeProps>> = forwardRef<
   typeof Box,
-  Partial<BadgeProps>
->((props, ref) => {
-  const tailwind = useTheme();
-  const badgeStyles = useTheme('badge');
-  const {
-    _badgeLibProps: { size, variant, themeColor },
-  } = useBadgeProps(props);
-  return (
-    <Box
-      ref={ref}
-      style={[
-        tailwind.style([
-          badgeStyles.baseContainer,
-          badgeStyles.containerVariants[variant][themeColor],
-          badgeStyles.size.container[size],
-        ]),
-        { ...props.containerStyle },
-      ]}
-    >
-      {typeof props.children === 'string' ? (
-        <Text
-          style={[
-            tailwind.style([
-              badgeStyles.size.text[size],
-              badgeStyles.textVariants[variant][themeColor],
-            ]),
-            { ...props.textStyle },
-          ]}
-        >
-          {props.children}
-        </Text>
+  BadgeProps
+>(
+  (
+    {
+      size = 'md',
+      themeColor = 'default',
+      variant = 'solid',
+      textProps = {},
+      prefix,
+      ...props
+    },
+    ref
+  ) => {
+    const tailwind = useTheme();
+
+    const badgeStyles = useTheme('badge');
+    const { style: boxStyle = {}, ...otherBoxProps } = props;
+    const { style: textStyle = {}, ...otherTextProps } = textProps;
+
+    const _prefix =
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      prefix?.type === Icon ? (
+        createIcon({
+          icon: prefix,
+          iconFill: tailwind.getColor(
+            badgeStyles.textVariants[variant][themeColor]
+          ),
+          iconStyle: tailwind.style(cx(badgeStyles.prefix.size[size])),
+        })
       ) : (
-        props.children
-      )}
-    </Box>
-  );
-});
+        <Box style={tailwind.style(cx(badgeStyles.prefix.size[size]))}>
+          {prefix}
+        </Box>
+      );
+
+    return (
+      <Box
+        ref={ref}
+        style={[
+          tailwind.style(
+            cx(
+              badgeStyles.baseContainer,
+              badgeStyles.containerVariants[variant][themeColor],
+              badgeStyles.size.container[size]
+            )
+          ),
+          styleAdapter(boxStyle, { pressed: false }, false),
+        ]}
+        {...otherBoxProps}
+      >
+        {prefix && _prefix}
+        {typeof props.children === 'string' ? (
+          <Text
+            style={[
+              tailwind.style(
+                cx(
+                  badgeStyles.size.text[size],
+                  badgeStyles.textVariants[variant][themeColor]
+                )
+              ),
+              styleAdapter(textStyle, { pressed: false }, false),
+            ]}
+            {...otherTextProps}
+          >
+            {props.children}
+          </Text>
+        ) : (
+          props.children
+        )}
+      </Box>
+    );
+  }
+);
 
 RNBadge.displayName = 'RNBadge';
 
