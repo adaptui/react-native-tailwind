e4b96446e22ed2b66a9b502c511229dbe7e31f37
Karthik-B-06 <karthik06051996@gmail.com>
2022-06-07 12:54:05 +0530

    chore: :sparkles: added BadgeScreen component

--- .ondevice/AppRoot.tsx
+++ .ondevice/AppRoot.tsx
@@ -1,6 +1,7 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import React from 'react';
-import { Avatar, Box, Button, useTheme } from '../src';
+import { Avatar, Badge, Box, Button, Icon, useTheme } from '../src';
+import { Check, Clock, Close } from '../src/assets';
 
 function ButtonScreen() {
   const tailwind = useTheme();
@@ -24,20 +25,67 @@ function AvatarScreen() {
   );
 }
 
+function BadgeScreen() {
+  const tailwind = useTheme();
+  return (
+    <Box
+      style={tailwind.style('flex-1 justify-center items-center bg-white-900')}
+    >
+      <Badge style={tailwind.style('my-1')} themeColor="secondary">
+        Scheduled
+      </Badge>
+      <Badge
+        style={tailwind.style('my-1')}
+        themeColor="primary"
+        variant="subtle"
+        prefix={<Icon icon={<Clock />} />}
+      >
+        On Progress
+      </Badge>
+      <Badge
+        style={tailwind.style('my-1')}
+        themeColor="danger"
+        prefix={<Icon icon={<Close />} />}
+      >
+        Cancelled
+      </Badge>
+      <Badge
+        style={tailwind.style('my-1')}
+        themeColor="success"
+        prefix={<Icon icon={<Check />} />}
+      >
+        Completed
+      </Badge>
+      <Badge
+        style={tailwind.style('my-1')}
+        variant="subtle"
+        themeColor="secondary"
+      >
+        Review
+      </Badge>
+    </Box>
+  );
+}
+
 const Drawer = createDrawerNavigator();
 
 const AppRoot = () => {
   return (
     <Drawer.Navigator initialRouteName="AvatarScreen">
       <Drawer.Screen
-        options={{ title: 'Avatar ' }}
+        options={{ title: 'Avatar' }}
         name="AvatarScreen"
         component={AvatarScreen}
       />
       <Drawer.Screen
-        options={{ title: 'Button ' }}
+        options={{ title: 'Button' }}
         name="ButtonScreen"
         component={ButtonScreen}
+      />
+      <Drawer.Screen
+        options={{ title: 'Badge ' }}
+        name="BadgeScreen"
+        component={BadgeScreen}
       />
     </Drawer.Navigator>
   );
