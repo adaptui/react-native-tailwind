65b6d544f600732f2c3ee3d837297d66f4cc7c3a
Karthik <35562287+Karthik-B-06@users.noreply.github.com>
2022-06-08 17:23:01 +0530

    chore: ✨ set showRing to true in AvatarGroup (#42)
    * chore: :sparkles: set showRing to true in AvatarGroup
    
    * chore: :sparkles: added AvatarGroup examples

--- example/src/modules/primitives/AvatarGroupScreen.tsx
+++ example/src/modules/primitives/AvatarGroupScreen.tsx
@@ -12,14 +12,58 @@ export default function AvatarGroupScreen() {
     <Box
       style={tailwind.style("flex-1 justify-center items-center bg-white-900")}
     >
-      <AvatarGroup>
-        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=1" }} />
-        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=2" }} />
-        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=3" }} />
-        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=4" }} />
-        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=5" }} />
+      <AvatarGroup size="md">
+        <Avatar />
+        <Avatar />
+        <Avatar />
+      </AvatarGroup>
+      <AvatarGroup size="xl">
+        <Avatar />
+        <Avatar />
+        <Avatar />
+        <Avatar />
+      </AvatarGroup>
+      <AvatarGroup size="2xl">
+        <Avatar name={"Navin Moorthy"} />
+        <Avatar name={"Karthik Balasubramanian"} />
+        <Avatar name={"Sandeep Prabhakaran"} />
+        <Avatar name={"Abhishek MG"} />
+        <Avatar name={"Udhaya Chandran"} />
+      </AvatarGroup>
+      <AvatarGroup size="2xl">
         <Avatar src={{ uri: "https://i.pravatar.cc/300??img=6" }} />
-        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=7" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=13" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=25" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=33" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=45" }} />
+      </AvatarGroup>
+      <AvatarGroup size="2xl" squared>
+        <Avatar name={"Navin Moorthy"} />
+        <Avatar name={"Karthik Balasubramanian"} />
+        <Avatar name={"Sandeep Prabhakaran"} />
+        <Avatar name={"Abhishek MG"} />
+        <Avatar name={"Udhaya Chandran"} />
+      </AvatarGroup>
+      <AvatarGroup max={2}>
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=6" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=13" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=25" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=33" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=45" }} />
+      </AvatarGroup>
+      <AvatarGroup max={3} squared>
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=6" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=13" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=25" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=33" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=45" }} />
+      </AvatarGroup>
+      <AvatarGroup squared>
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=6" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=13" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=25" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=33" }} />
+        <Avatar src={{ uri: "https://i.pravatar.cc/300??img=45" }} />
       </AvatarGroup>
     </Box>
   );
