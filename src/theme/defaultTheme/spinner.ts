f569817561a09cd6f0c80b726904c54debb580b0
Karthik-B-06 <karthik06051996@gmail.com>
2022-06-13 20:18:07 +0530

    chore(spinner): :sparkles:  wrap spinner component - add theming to spinner, - wrap tailwind classnames inside `cx` & - add description to spinner props

--- src/theme/defaultTheme/spinner.ts
+++ src/theme/defaultTheme/spinner.ts
@@ -1,8 +1,21 @@
 export const spinner = {
-  base: "border-2 border-solid rounded-full border-gray-800",
+  base: "border-2 border-solid rounded-full",
   track: {
     transparent: "border-b-transparent border-l-transparent",
-    visible: "border-b-gray-300 border-l-gray-300",
+    visible: {
+      base: "border-b-gray-400 border-l-gray-400",
+      primary: "border-b-blue-400 border-l-blue-400",
+      secondary: "border-b-violet-400 border-l-violet-400",
+      success: "border-b-green-400 border-l-green-400",
+      danger: "border-b-red-400 border-l-red-400",
+    },
+  },
+  themeColor: {
+    base: "border-gray-800",
+    primary: "border-blue-800",
+    secondary: "border-violet-800",
+    success: "border-green-800",
+    danger: "border-red-800",
   },
   label: "sr-only",
   size: {
