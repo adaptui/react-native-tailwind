3cca1400a4ae176006e7ba8eb4724e9be6ebc945
Karthik-B-06 <karthik06051996@gmail.com>
2022-06-14 12:06:35 +0530

    chore(badge): :ambulance: fix outline text color, add vertical padding to badge container

--- src/theme/defaultTheme/badge.ts
+++ src/theme/defaultTheme/badge.ts
@@ -12,7 +12,7 @@ export const badge = {
       },
       outline: {
         container: "bg-white-900 border border-gray-400",
-        text: "text-gray-600",
+        text: "text-gray-800",
       },
     },
     primary: {
@@ -26,7 +26,7 @@ export const badge = {
       },
       outline: {
         container: "bg-white-900 border border-blue-400",
-        text: "text-blue-500",
+        text: "text-blue-800",
       },
     },
     secondary: {
@@ -40,7 +40,7 @@ export const badge = {
       },
       outline: {
         container: "bg-white-900 border border-violet-400",
-        text: "text-violet-500",
+        text: "text-violet-800",
       },
     },
     success: {
@@ -54,7 +54,7 @@ export const badge = {
       },
       outline: {
         container: "bg-white-900 border border-green-400",
-        text: "text-green-500",
+        text: "text-green-800",
       },
     },
     danger: {
@@ -68,23 +68,23 @@ export const badge = {
       },
       outline: {
         container: "bg-white-900 border border-red-400",
-        text: "text-red-500",
+        text: "text-red-800",
       },
     },
   },
   size: {
     sm: {
-      container: "px-1.5 min-h-4",
+      container: "px-1.5 py-px min-h-4",
       text: "text-xs text-center font-medium",
       prefix: "w-2 mr-1 items-center justify-center",
     },
     md: {
-      container: "px-1.5 min-h-5",
+      container: "px-1.5 py-[3px] min-h-5",
       text: "text-xs text-center font-medium",
       prefix: "w-2.5 mr-1 items-center justify-center",
     },
     lg: {
-      container: "px-2 min-h-6",
+      container: "px-2 py-[4.5px] min-h-6",
       text: "text-cxs text-center font-medium",
       prefix: "w-3 mr-1 items-center justify-center",
     },
