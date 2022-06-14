647478b465e7d84cded0776383f5280df7cc8e2f
Karthik-B-06 <karthik06051996@gmail.com>
2022-06-14 15:58:47 +0530

    chore(spinner): :adhesive_bandage: change `h-14px` tailwind classname to `h-3.5`

--- src/theme/defaultTheme/spinner.ts
+++ src/theme/defaultTheme/spinner.ts
@@ -20,7 +20,7 @@ export const spinner = {
   label: "sr-only",
   size: {
     xs: "h-3 w-3",
-    sm: "h-[14px] w-[14px]",
+    sm: "h-3.5 w-3.5",
     md: "h-4 w-4",
     lg: "h-5 w-5",
     xl: "h-6 w-6",
