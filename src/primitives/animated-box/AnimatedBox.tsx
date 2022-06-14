789e8574ef8734f0c1724542c35c15cc0a811718
Karthik-B-06 <karthik06051996@gmail.com>
2022-06-14 16:06:32 +0530

    chore(primitives): :truck: move primitive components back to original name

--- src/primitives/animated-box/AnimatedBox.tsx
+++ src/primitives/animated-box/AnimatedBox.tsx
@@ -1,7 +1,7 @@
 import Animated from "react-native-reanimated";
 
 import { createComponent } from "../../utils";
-import { Box } from "../box-temp";
+import { Box } from "../box";
 
 // @ts-ignore
 const RNAnimatedBox = Animated.createAnimatedComponent(Box);
