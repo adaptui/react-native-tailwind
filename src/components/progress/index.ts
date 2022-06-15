3f17bcf9df6ea93711e167b2203a926787b7fef7
Karthik-B-06 <karthik06051996@gmail.com>
2022-06-15 20:56:20 +0530

    chore(progress-bar): :sparkles: wrap progress bar component - add theming to progress bar - restructure progress bar theme file - add props for customising progress bar

--- src/components/progress/index.ts
+++ src/components/progress/index.ts
@@ -1,2 +1,1 @@
 export * from "./ProgressBar";
-export * from "./ProgressProps";
--- src/theme/defaultTheme/progress.ts
+++ src/theme/defaultTheme/progress.ts
@@ -1,16 +1,30 @@
 export const progress = {
-  container: {
-    sm: "w-full h-0.5 rounded-full overflow-hidden",
-    md: "w-full h-1 rounded-full overflow-hidden",
-    lg: "w-full h-2 rounded-full overflow-hidden",
-    xl: "w-full h-3 rounded-full overflow-hidden",
+  size: {
+    sm: {
+      container: "w-full h-0.5 rounded-full overflow-hidden",
+      bar: "absolute h-0.5 rounded-full",
+    },
+    md: {
+      container: "w-full h-1 rounded-full overflow-hidden",
+      bar: "absolute h-1 rounded-full",
+    },
+    lg: {
+      container: "w-full h-2 rounded-full overflow-hidden",
+      bar: "absolute h-2 rounded-full",
+    },
+    xl: {
+      container: "w-full h-3 rounded-full overflow-hidden",
+      bar: "absolute h-3 rounded-full",
+    },
   },
-  bar: {
-    sm: "absolute h-0.5 rounded-full",
-    md: "absolute h-1 rounded-full",
-    lg: "absolute h-2 rounded-full",
-    xl: "absolute h-3 rounded-full",
+  themeColor: {
+    base: {
+      track: "bg-gray-100",
+      filled: "bg-gray-900",
+    },
+    primary: {
+      track: "bg-blue-100",
+      filled: "bg-blue-600",
+    },
   },
-  defaultTrackColor: "bg-gray-200",
-  defaultProgressTrackColor: "bg-gray-800",
 };
