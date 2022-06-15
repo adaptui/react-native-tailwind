d760493f395b91e357521172129a8ffb44a17b05
Karthik-B-06 <karthik06051996@gmail.com>
2022-06-15 19:30:57 +0530

    chore(tag): :sparkles: add themeColor for tag, remove TagProps hook, restructured tag render

--- src/components/tag/__keys.ts
+++
@@ -1,2 +0,0 @@
-export const TAG_LIB_KEYS = ["size", "variant"];
-export const TAG_OPTION_KEYS = ["prefix", "suffix", "closable"];
--- src/theme/defaultTheme/tag.ts
+++ src/theme/defaultTheme/tag.ts
@@ -1,76 +1,165 @@
 export const tag = {
   base: "flex flex-row items-center justify-center relative",
-  variant: {
-    default: {
-      solid: "bg-gray-800 border border-transparent",
-      subtle: "bg-gray-100 border border-transparent",
-      outline: "bg-white border border-gray-200",
-    },
-    disabled: {
-      solid: "bg-gray-200 border border-transparent",
-      subtle: "bg-gray-100 border border-transparent",
-      outline: "bg-white border border-gray-200",
-    },
-    icon: {
-      default: {
-        solid: "text-gray-400",
-        subtle: "text-gray-500",
-        outline: "text-gray-400",
+  themeColor: {
+    base: {
+      solid: {
+        container: {
+          wrapper: "bg-gray-900 border border-transparent",
+          pressed: "bg-gray-700",
+          disabled: "bg-gray-200",
+        },
+        text: {
+          default: "text-white-900",
+          disabled: "bg-gray-200 text-gray-500",
+        },
+        icon: {
+          default: "text-white-900",
+          disabled: "text-gray-500",
+        },
+        spinner: {
+          disabled: "border-gray-500",
+        },
       },
-      disabled: {
-        solid: "text-gray-400",
-        subtle: "text-gray-400",
-        outline: "text-gray-400",
+      subtle: {
+        container: {
+          wrapper: "bg-gray-100 border border-transparent",
+          pressed: "bg-gray-300",
+          disabled: "bg-gray-100",
+        },
+        text: {
+          default: "text-gray-800",
+          disabled: "bg-gray-100 text-gray-500",
+        },
+        icon: {
+          default: "text-gray-800",
+          disabled: "text-gray-500",
+        },
+        spinner: {
+          disabled: "border-gray-500",
+        },
+      },
+      outline: {
+        container: {
+          wrapper: "bg-white-900 border border-gray-400",
+          pressed: "border-gray-500 bg-gray-300",
+          disabled: "bg-gray-100 border-gray-400",
+        },
+        text: {
+          default: "text-gray-800",
+          disabled: "bg-gray-100 text-gray-500",
+        },
+        icon: {
+          default: "text-gray-800",
+          disabled: "text-gray-500",
+        },
+        spinner: {
+          disabled: "border-gray-500",
+        },
       },
     },
-    text: {
-      default: {
-        solid: "text-white",
-        subtle: "text-gray-600",
-        outline: "text-gray-600",
+    primary: {
+      solid: {
+        container: {
+          wrapper: "bg-blue-600 border border-transparent",
+          pressed: "bg-blue-800",
+          disabled: "bg-blue-100",
+        },
+        text: {
+          default: "text-white-900",
+          disabled: "text-blue-500",
+        },
+        icon: {
+          default: "text-white-900",
+          disabled: "text-blue-500",
+        },
+        spinner: {
+          disabled: "border-blue-500",
+        },
       },
-      disabled: {
-        solid: "text-gray-500",
-        subtle: "text-gray-400",
-        outline: "text-gray-400",
+      subtle: {
+        container: {
+          wrapper: "bg-blue-100 border border-transparent",
+          pressed: "bg-blue-300",
+          disabled: "bg-blue-100",
+        },
+        text: {
+          default: "text-blue-800",
+          disabled: "text-blue-500",
+        },
+        icon: {
+          default: "text-blue-800",
+          disabled: "text-blue-500",
+        },
+        spinner: {
+          disabled: "border-blue-500",
+        },
       },
-    },
-    pressed: {
-      solid: "bg-gray-900",
-      subtle: "bg-gray-200",
-      outline: "bg-gray-100 border-gray-300 shadow-sm",
+      outline: {
+        container: {
+          wrapper: "bg-white-900 border border-blue-400",
+          pressed: "border-blue-500 bg-blue-300",
+          disabled: "bg-blue-100 border-blue-400",
+        },
+        text: {
+          default: "text-blue-800",
+          disabled: "text-blue-500",
+        },
+        icon: {
+          default: "text-blue-800",
+          disabled: "text-blue-500",
+        },
+        spinner: {
+          disabled: "border-blue-500",
+        },
+      },
+      ghost: {
+        container: {
+          wrapper: "bg-white-900 border border-transparent",
+          pressed: "bg-blue-300",
+          disabled: "",
+        },
+        text: {
+          default: "text-blue-800",
+          disabled: "text-blue-500",
+        },
+        icon: {
+          default: "text-blue-800",
+          disabled: "text-blue-500",
+        },
+        spinner: {
+          disabled: "border-blue-500",
+        },
+      },
     },
   },
   size: {
-    default: {
-      sm: "min-h-5 w-auto min-w-5 px-1.5 rounded-[5px]",
-      md: "min-h-6 w-auto min-w-6 px-2 rounded-md",
-      lg: "min-h-7 w-auto min-w-7 px-2 rounded-md",
-      xl: "min-h-9 w-auto min-w-9 px-3 rounded-lg",
+    sm: {
+      default: "min-h-5 w-auto min-w-5 px-1.5 rounded-[5px]",
+      prefix: "w-3 mr-1.5 items-center justify-center",
+      suffix: "w-3 ml-1.5 items-center justify-center",
+      closable: "w-3 ml-1",
+      text: "text-cxs font-medium",
     },
-    prefix: {
-      sm: "w-3 mr-1.5 items-center justify-center",
-      md: "w-3 mr-1.5 items-center justify-center",
-      lg: "w-3 mr-1.5 items-center justify-center",
-      xl: "w-4 mr-1.5 items-center justify-center",
+    md: {
+      default: "min-h-6 w-auto min-w-6 px-2 rounded-md",
+      prefix: "w-3 mr-1.5 items-center justify-center",
+      suffix: "w-3 ml-1.5 items-center justify-center",
+      closable: "w-3 ml-1",
+      text: "text-cxs font-medium",
     },
-    suffix: {
-      sm: "w-3 ml-1.5 items-center justify-center",
-      md: "w-3 ml-1.5 items-center justify-center",
-      lg: "w-3 ml-1.5 items-center justify-center",
-      xl: "w-4 ml-1.5 items-center justify-center",
+    lg: {
+      default: "min-h-7 w-auto min-w-7 px-2 rounded-md",
+      prefix: "w-3 mr-1.5 items-center justify-center",
+      suffix: "w-3 ml-1.5 items-center justify-center",
+      closable: "w-3 ml-1",
+      text: "text-sm font-medium",
     },
-    closable: {
-      sm: "w-3 ml-1",
-      md: "w-3 ml-1",
-      lg: "w-3 ml-1",
-      xl: "w-4 ml-1.5",
-    },
-    text: {
-      sm: "text-cxs font-medium",
-      md: "text-cxs font-medium",
-      lg: "text-sm font-medium",
-      xl: "text-base font-medium",
+    xl: {
+      default: "min-h-9 w-auto min-w-9 px-3 rounded-lg",
+      prefix: "w-4 mr-1.5 items-center justify-center",
+      suffix: "w-4 ml-1.5 items-center justify-center",
+      closable: "w-4 ml-1.5",
+      text: "text-base font-medium",
     },
   },
 };
