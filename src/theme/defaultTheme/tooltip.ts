51b13dcf62ee27966517ba3da47d294754086593
Karthik-B-06 <karthik06051996@gmail.com>
2022-06-15 17:23:03 +0530

    chore(tooltip): :lipstick: updated tooltip theme

--- src/theme/defaultTheme/tooltip.ts
+++ src/theme/defaultTheme/tooltip.ts
@@ -1,5 +1,5 @@
 export const tooltip = {
   contentWrapper:
-    "relative items-center justify-center px-2 py-[6.5px] bg-gray-800 rounded-lg shadow-lg max-w-[160px]",
-  content: "text-cxs text-white font-normal leading-4 text-center",
+    "relative items-center justify-center px-2 py-[5.5px] bg-gray-900 rounded-lg shadow-lg max-w-[160px]",
+  content: "text-cxs text-white-900 font-normal leading-4 text-center",
 };
