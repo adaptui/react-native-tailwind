785023e1f9909688c9161e8523fca2bf3bc332be
Karthik-B-06 <karthik06051996@gmail.com>
2022-07-01 11:50:16 +0530

    chore(example-app): :zap: update example app with Radio Group Example

--- example/src/AppRoot.tsx
+++ example/src/AppRoot.tsx
@@ -24,7 +24,7 @@ const Drawer = createDrawerNavigator();
 
 const AppRoot = () => {
   return (
-    <Drawer.Navigator initialRouteName="CheckboxGroupScreen">
+    <Drawer.Navigator initialRouteName="RadioComponentScreen">
       <Drawer.Screen
         options={{ title: "Avatar" }}
         name="AvatarScreen"
