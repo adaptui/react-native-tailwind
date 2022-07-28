d20aba1e1a83b35553d3a757ac08169debe9365c
Karthik-B-06 <karthik06051996@gmail.com>
2022-07-28 11:09:19 +0530

    feat: :sparkles: add nativewind styled function wrapper to Text

--- src/primitives/text/Text.tsx
+++ src/primitives/text/Text.tsx
@@ -1,4 +1,5 @@
 import { Text as RNText } from "react-native";
+import { styled } from "nativewind";
 
 import {
   As,
@@ -9,15 +10,17 @@ import {
   Props,
 } from "../../utils/system";
 
+export const StyledText = styled(RNText);
+
 export const useText = createHook<TextOptions>(props => {
   return props;
 });
 
 export const Text = createComponent<TextOptions>(props => {
   const htmlProps = useText(props);
-  return createElement(RNText, htmlProps);
+  return createElement(StyledText, htmlProps);
 });
 
-export type TextOptions<T extends As = typeof RNText> = Options<T>;
+export type TextOptions<T extends As = typeof StyledText> = Options<T>;
 
-export type TextProps<T extends As = typeof RNText> = Props<TextOptions<T>>;
+export type TextProps<T extends As = typeof StyledText> = Props<TextOptions<T>>;
