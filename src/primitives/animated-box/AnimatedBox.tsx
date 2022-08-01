d8f2b713452b738450a088c6e6d72d74af81a355
Karthik-B-06 <karthik06051996@gmail.com>
2022-08-01 13:55:49 +0530

    feat(primitives): :tada: create AnimatedBox using hook based approach

--- src/primitives/animated-box/AnimatedBox.tsx
+++ src/primitives/animated-box/AnimatedBox.tsx
@@ -1,9 +1,32 @@
 import Animated from "react-native-reanimated";
 
-import { createComponent } from "../../utils";
+import {
+  As,
+  ComponentOptions,
+  createComponent,
+  createElement,
+  createHook,
+  Props,
+} from "../../utils/system";
 import { Box } from "../box";
 
 // @ts-ignore
 const RNAnimatedBox = Animated.createAnimatedComponent(Box);
 
-export const AnimatedBox = createComponent(RNAnimatedBox, { shouldMemo: true });
+export const useAnimatedBox = createHook<AnimatedBoxOptions>(
+  ({ __TYPE__, ...props }) => {
+    return props;
+  },
+);
+
+export const AnimatedBox = createComponent<AnimatedBoxOptions>(props => {
+  const htmlProps = useAnimatedBox(props);
+  return createElement(RNAnimatedBox, htmlProps);
+});
+
+export type AnimatedBoxOptions<T extends As = typeof RNAnimatedBox> =
+  ComponentOptions<T>;
+
+export type AnimatedBoxProps<T extends As = typeof RNAnimatedBox> = Props<
+  AnimatedBoxOptions<T>
+>;
