5c9aa3389ab2a63612894517ab8beff2b1351ca7
criston <criston@timeless.co>
2022-11-01 20:24:27 +0530

    fix: divider style

--- example/src/modules/primitives/DividerScreen.tsx
+++ example/src/modules/primitives/DividerScreen.tsx
@@ -4,20 +4,22 @@ import { Box, Divider, useTheme } from "@adaptui/react-native-tailwind";
 export const DividerScreen = () => {
   const tailwind = useTheme();
   return (
-    <Box
-      style={tailwind.style("flex-1 justify-center items-center bg-white-900")}
-    >
-      <Box style={tailwind.style("w-full ")}>
+    <Box style={tailwind.style("flex-1 justify-center items-center ")}>
+      <Box
+        style={tailwind.style(
+          "w-full bg-white-900 border rounded-xl h-40 justify-center",
+        )}
+      >
         <Divider onPress={() => console.log("hello")} />
       </Box>
       <Box
         style={tailwind.style(
-          "h-100 overflow-hidden items-center justify-center",
+          "h-100 overflow-hidden items-center justify-center border mt-3 rounded-xl w-80 bg-white-900 ",
         )}
       >
         <Divider
           orientation="vertical"
-          inset={20}
+          inset={40}
           insetType="top"
           onPress={() => console.log("hello")}
         />
