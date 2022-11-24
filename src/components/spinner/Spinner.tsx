2f2953df207fd960211dccd628f86f3d6443a091
crstnmac <criston@timeless.co>
2022-11-24 15:45:47 +0530

    feat(example-app): :sparkles: add prop controls to spinner view screen

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -37,7 +37,7 @@ export interface SpinnerProps extends BoxProps {
    * How the spinner should be displayed?
    * @default transparent
    */
-  track: "visible" | "transparent";
+  track: SpinnerTrackVisibility;
   /**
    * Spinner Theme
    * @default base
