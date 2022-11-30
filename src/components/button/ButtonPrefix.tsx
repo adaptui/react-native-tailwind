8ee1197a2e85433e04d9fbdf48ebf608211f541c
Karthik-B-06 <karthik06051996@gmail.com>
2022-11-30 12:06:55 +0530

    fix(types): :label: map suffix, prefix and spinner render components type to ReactNode

--- src/components/button/ButtonPrefix.tsx
+++ src/components/button/ButtonPrefix.tsx
@@ -8,18 +8,19 @@ import { ButtonSizes } from "./Button";
 
 export interface ButtonPrefixProps {
   size: ButtonSizes;
+  children: React.ReactNode;
 }
 
 export const ButtonPrefix: React.FC<ButtonPrefixProps> = ({
   size,
-  ...props
+  children,
 }) => {
   const tailwind = useTheme();
   const buttonTheme = useTheme("button");
 
   return (
     <Box style={[tailwind.style(cx(buttonTheme.size[size]?.prefix))]}>
-      {props.children}
+      {children}
     </Box>
   );
 };
--- src/components/button/ButtonSuffix.tsx
+++ src/components/button/ButtonSuffix.tsx
@@ -8,18 +8,19 @@ import { ButtonSizes } from "./Button";
 
 export interface ButtonSuffixProps {
   size: ButtonSizes;
+  children: React.ReactNode;
 }
 
 export const ButtonSuffix: React.FC<ButtonSuffixProps> = ({
   size,
-  ...props
+  children,
 }) => {
   const tailwind = useTheme();
   const buttonTheme = useTheme("button");
 
   return (
     <Box style={[tailwind.style(cx(buttonTheme.size[size]?.suffix))]}>
-      {props.children}
+      {children}
     </Box>
   );
 };
