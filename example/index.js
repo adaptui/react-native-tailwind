50c07779c81e2cc53e66adc3b36783315144621b
crstnmac <criston2011@gmail.com>
2023-01-10 17:07:56 +0530

    fix(example-app): :bug: fix react18 error for rn-web

--- example/index.js
+++ example/index.js
@@ -1,9 +1,20 @@
+import { createElement } from "react";
+import { createRoot } from "react-dom/client";
+import { Platform } from "react-native";
 import { registerRootComponent } from "expo";
 
 import App from "./src/App";
 
 import "react-native-gesture-handler";
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+if (Platform.OS === "web") {
+  const rootTag = createRoot(
+    document.getElementById("root") ?? document.getElementById("main"),
+  );
+  rootTag.render(createElement(App));
+} else {
+  registerRootComponent(App);
+}
