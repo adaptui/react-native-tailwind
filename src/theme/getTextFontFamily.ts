c3c93320a7ea92ddb221e0b802fde38f2ef6a17e
Karthik-B-06 <karthik06051996@gmail.com>
2023-01-17 13:38:21 +0530

    feat(utils): :sparkles: add getTextLineHeight() for android fixing <Text> issues

--- src/theme/getTextFontFamily.ts
+++ src/theme/getTextFontFamily.ts
@@ -1,6 +1,6 @@
 import { Platform, TextStyle } from "react-native";
 /**
- * This hook is to switch between font family based on Platform
+ * This function is to switch between font family based on Platform
  * iOS using System Font there is no need to set is explicitly
  * For Android, we use Inter
  * font-thin - 100;
@@ -23,6 +23,23 @@ type FONT_FAMILY_KEY =
   | "font-semibold"
   | "font-bold"
   | "font-extrabold";
+
+type FONT_SIZE_KEY =
+  | "text-xs"
+  | "text-cxs"
+  | "text-sm"
+  | "text-base"
+  | "text-lg"
+  | "text-xl"
+  | "text-cxl"
+  | "text-2xl"
+  | "text-3xl"
+  | "text-4xl"
+  | "text-5xl"
+  | "text-6xl"
+  | "text-7xl"
+  | "text-8xl"
+  | "text-9xl";
 
 export type ObjectKeys<T extends object> = keyof T;
 
@@ -53,6 +70,60 @@ export const getTextFontFamily = (tClassNames: string): TextStyle => {
         }
       : {
           fontFamily: fontFamily ? fontFamilies[fontFamily] : "Inter-Regular",
+        };
+  return textStyle;
+};
+
+/**
+ * This function is to switch between font line height based on font size
+ * in Platform Android
+ * Mapped values based on the Text Size from Tailwind CSS
+ * text-xs - 14,
+ * text-cxs - 15,
+ * text-sm - 16,
+ * text-base - 18,
+ * text-lg - 21,
+ * text-xl - 23,
+ * text-cxl - 25,
+ * text-2xl - 28,
+ * text-3xl - 34,
+ * text-4xl - 41,
+ * text-5xl - 55,
+ * text-6xl - 69,
+ * text-7xl - 83,
+ * text-8xl - 110,
+ * text-9xl - 147,
+ */
+
+export const getTextLineHeight = (tClassNames: string): TextStyle => {
+  const lineHeights: Record<FONT_SIZE_KEY, number> = {
+    "text-xs": 14,
+    "text-cxs": 15,
+    "text-sm": 16,
+    "text-base": 18,
+    "text-lg": 21,
+    "text-xl": 23,
+    "text-cxl": 25,
+    "text-2xl": 28,
+    "text-3xl": 34,
+    "text-4xl": 41,
+    "text-5xl": 55,
+    "text-6xl": 69,
+    "text-7xl": 83,
+    "text-8xl": 110,
+    "text-9xl": 147,
+  };
+
+  const tailwindClass = tClassNames.split(" ");
+  const keys = objectKeys(lineHeights);
+  const lineHeight = keys.find(className => tailwindClass.includes(className));
+  const textStyle: TextStyle =
+    Platform.OS === "ios"
+      ? {}
+      : {
+          lineHeight: lineHeight ? lineHeights[lineHeight] : undefined,
+          textAlignVertical: "center",
+          includeFontPadding: false,
         };
   return textStyle;
 };
