05d9b4f679c89d13723ce29ae25cac65011170e0
Karthik-B-06 <karthik06051996@gmail.com>
2023-01-17 17:29:11 +0530

    refactor(select): :bug: add optional chaining operator (?) while accessing the select theme object

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -181,14 +181,14 @@ const RNSelect: React.FC<Partial<SelectProps>> = forwardRef<
         style={(touchState: PressableStateCallbackType) => [
           ts([
             cx(
-              selectStyle.base.common,
-              selectStyle.base.size[size].common,
-              !prefix ? selectStyle.base.size[size].withoutAddon : "",
-              selectStyle.base.variant[variant].default,
-              invalid ? selectStyle.base.variant[variant].invalid : "",
-              disabled ? selectStyle.base.variant[variant].disabled : "",
+              selectStyle.base?.common,
+              selectStyle.base?.size[size]?.common,
+              !prefix ? selectStyle.base?.size[size]?.withoutAddon : "",
+              selectStyle.base?.variant[variant]?.default,
+              invalid ? selectStyle.base?.variant[variant]?.invalid : "",
+              disabled ? selectStyle.base?.variant[variant]?.disabled : "",
               touchState.pressed || hovered.value
-                ? selectStyle.base.variant[variant].pressedOrHovered
+                ? selectStyle.base?.variant[variant]?.pressedOrHovered
                 : "",
             ),
           ]),
@@ -217,21 +217,21 @@ const RNSelect: React.FC<Partial<SelectProps>> = forwardRef<
                 style={[
                   ts([
                     cx(
-                      selectStyle.base.size[size].text,
+                      selectStyle.base?.size[size]?.text,
                       touchState.pressed || hovered.value
-                        ? selectStyle.base.variant[variant].text
-                            .pressedOrHovered
+                        ? selectStyle.base?.variant[variant]?.text
+                            ?.pressedOrHovered
                         : disabled
-                        ? selectStyle.base.variant[variant].text.disabled
+                        ? selectStyle.base?.variant[variant]?.text?.disabled
                         : isUndefined(selectState)
-                        ? selectStyle.base.variant[variant].text.default
-                        : selectStyle.base.variant[variant].text.filled,
+                        ? selectStyle.base?.variant[variant]?.text?.default
+                        : selectStyle.base?.variant[variant]?.text?.filled,
                       prefix ? `pl-[${prefixWidth}px]` : "",
                       `pr-[${suffixWidth}px]`,
                     ),
                   ]),
-                  getTextFontFamily(selectStyle.base.size[size].text),
-                  getTextLineHeight(selectStyle.base.size[size].text),
+                  getTextFontFamily(selectStyle.base?.size[size]?.text),
+                  getTextLineHeight(selectStyle.base?.size[size]?.text),
                 ]}
               >
                 {isUndefined(selectState)
